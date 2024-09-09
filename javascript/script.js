a10edb661db85ee037b53b8ca32bfa84be3e5718
reshuk-code <reshuksapkota2007@gmail.com>
2024-09-09 18:26:18 +0545

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.getElementById('navbarLinks').classList.toggle('show');
+  });
+  
